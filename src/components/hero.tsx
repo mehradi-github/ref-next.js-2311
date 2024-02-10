da0aa2a4cc037c981badb4ea042278b2445b347a
Mehradi <mehradi.info@gmail.com>
2024-02-10 09:41:51 +0100

    Adding gradient to Hero section

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -11,6 +11,7 @@ const Hero: FC<Iprop> = ({ src, alt, title }: Iprop) => {
     <div className="relative h-screen">
       <div className="absolute -z-10 inset-0">
         <Image fill={true} src={src} alt={alt} style={{ objectFit: "cover" }} />
+        <div className="absolute inset-0 bg-gradient-to-r from-slate-900" />
       </div>
       <div className="pt-48 flex justify-center items-center">
         <h1 className="text-white text-6xl">{title}</h1>
