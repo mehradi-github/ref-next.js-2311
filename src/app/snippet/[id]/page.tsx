fe03206543768bafb3db632ff64bb5659b5aa33f
Mehradi <mehradi.info@gmail.com>
2024-02-14 11:26:28 +0100

    Adding custom loading and not found page

--- src/app/snippet/[id]/page.tsx
+++ src/app/snippet/[id]/page.tsx
@@ -7,6 +7,7 @@ interface SnippetShowPageProps {
   };
 }
 const page = async (props: SnippetShowPageProps) => {
+  await new Promise((r) => setTimeout(r, 2000));
   const snippet = await db.snippet.findFirst({
     where: { id: parseInt(props.params.id) },
   });
---
+++ src/app/snippet/not-found.tsx
@@ -0,0 +1,4 @@
+const SnippetNotfound = () => {
+  return <div>Sorry, Not Found | 404</div>;
+};
+export default SnippetNotfound;
