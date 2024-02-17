660b859ef363e43a2854909e39637bdb8fa5df9d
Mehradi <mehradi.info@gmail.com>
2024-02-17 08:23:15 +0100

    Feching snippt from database via id

--- src/app/snippets/[id]/edit/page.tsx
+++ src/app/snippets/[id]/edit/page.tsx
@@ -1,3 +1,6 @@
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+
 interface SnippetEditPageProps {
   params: {
     id: string;
@@ -5,6 +8,10 @@ interface SnippetEditPageProps {
 }
 const SnippetEditPage = (props: SnippetEditPageProps) => {
   const id = parseInt(props.params.id);
+  const snippet = db.snippet.findFirst({
+    where: { id: id },
+  });
+  if (!snippet) return notFound();
   return <div>Edit {id}</div>;
 };
 export default SnippetEditPage;
