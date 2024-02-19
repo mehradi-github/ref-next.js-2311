f49703c6414c3a34dd5032c80714435586ffe8fb
Mehradi <mehradi.info@gmail.com>
2024-02-19 09:56:05 +0100

    Updating database in server action editSnippet and redirect

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,12 @@
 "use server";
 import { db } from "@/db";
+import { redirect } from "next/navigation";
 
 export const editSnippet = async (id: number, code: string) => {
   console.log(id, code);
+  await db.snippet.update({
+    where: { id },
+    data: { code },
+  });
+  redirect(`/snippets/${id}`);
 };
