218fc1798ae30bb8d9fa35d0a89d4005c561a877
Mehradi <mehradi.info@gmail.com>
2024-03-05 17:56:28 +0100

    Adding CredentialsProvider and GithubProvider in NextAuth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,5 @@
-export { GET, POST } from "@/auth";
+import NextAuth from "next-auth";
+import { options } from "./options";
+
+const handler = NextAuth(options);
+export { handler as GET, handler as PSOT };
