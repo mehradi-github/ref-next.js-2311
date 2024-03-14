e9a11099b83d3b7303c5634ef1ab43867afb977f
Mehradi <mehradi.info@gmail.com>
2024-03-14 12:59:07 +0100

    Fix error on build project

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -3,9 +3,10 @@ import GitHubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "@/db";
+import { Adapter } from "next-auth/adapters";
 
 export const options: NextAuthOptions = {
-  adapter: PrismaAdapter(db),
+  adapter: PrismaAdapter(db) as Adapter,
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID as string,
@@ -42,24 +43,23 @@ export const options: NextAuthOptions = {
   ],
   session: {
     // Set the session strategy to 'jwt'
-    strategy: "jwt",
-    maxAge: 30 * 24 * 60 * 60, // 30 days in seconds
+    strategy: "database",
+    //maxAge: 30 * 24 * 60 * 60, // 30 days in seconds
   },
   callbacks: {
-    async jwt({ token, account, profile }) {
-      // Persist the OAuth access_token and or the user id to the token right after signin
-      if (account) {
-        token.accessToken = account.access_token;
-        token.id = profile.id;
-      }
-      return token;
-    },
-    async session({ session, token, user }) {
-      // Send properties to the client, like an access_token and user id from a provider.
-      session.accessToken = token.accessToken;
-      session.user.id = token.id;
-
-      return session;
-    },
+    // async jwt({ token, account, profile }) {
+    //   // Persist the OAuth access_token and or the user id to the token right after signin
+    //   if (account) {
+    //     token.accessToken = account.access_token;
+    //     token.id = profile.id;
+    //   }
+    //   return token;
+    // },
+    // async session({ session, token, user }) {
+    //   // Send properties to the client, like an access_token and user id from a provider.
+    //   session.accessToken = token.accessToken;
+    //   session.user.id = token.id;
+    //   return session;
+    // },
   },
 };
