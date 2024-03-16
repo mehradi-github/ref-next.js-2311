fd38a31e9f4fb961293f9da757b81916fef0e5fe
Mehradi <mehradi.info@gmail.com>
2024-03-16 10:27:32 +0100

    Adding popover TopicCreateForm

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,15 +1,6 @@
 import TopicCreateForm from "@/components/topics/topic-create-form";
 
 const TopicShowPage = () => {
-  return (
-    <div className="grid grid-cols-4 gap-4 p-4">
-      <div className="col-span-3">
-        <h1 className="text-xl m-2"> Top Posts</h1>
-      </div>
-      <div>
-        <TopicCreateForm />
-      </div>
-    </div>
-  );
+  return <div>Topic</div>;
 };
 export default TopicShowPage;
--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -1,6 +1,35 @@
 "use client";
 
+import {
+  Button,
+  Input,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  Textarea,
+} from "@nextui-org/react";
+
 const TopicCreateForm = ({}) => {
-  return <div>TopicCreateForm</div>;
+  return (
+    <Popover placement="left">
+      <PopoverTrigger>
+        <Button color="primary">Create a Topic</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <form>
+          <div className="flex flex-col gap-4 p-4 w-80">
+            <h3 className="text-lg">Create a Topic</h3>
+            <Input label="Name" labelPlacement="outside" placeholder="Name" />
+            <Textarea
+              label="Discription"
+              labelPlacement="outside"
+              placeholder="Descripe your Topic"
+            />
+            <Button type="submit">Submit</Button>
+          </div>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
 };
 export default TopicCreateForm;
