f74c47f0f63b7ae9c3e40167d6d961d0aedfbea7
Mehradi <mehradi.info@gmail.com>
2024-03-17 19:59:50 +0100

    Receiving TopicCreateForm Data

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,2 +1,6 @@
 "use server";
-export const createTopic = async () => {};
+export const createTopic = async (formData: FormData) => {
+  const name = formData.get("name");
+  const discription = formData.get("discription");
+  console.log(name, discription);
+};
