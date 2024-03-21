29caf72295df4f0c923d94d3159af2621f5f8eee
Mehradi <mehradi.info@gmail.com>
2024-03-21 11:24:57 +0100

    Scaffolding the Topic Show Page

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,6 +1,17 @@
-import TopicCreateForm from "@/components/topics/topic-create-form";
+interface TopicShowPageProps {
+  params: {
+    slug: string;
+  };
+}
 
-const TopicShowPage = () => {
-  return <div>Topic</div>;
+const TopicShowPage = ({ params }: TopicShowPageProps) => {
+  const { slug } = params;
+  return (
+    <div>
+      <div className="cpl-span-3">
+        <h1 className="text-2xl font-bold mb-2">{slug}</h1>
+      </div>
+    </div>
+  );
 };
 export default TopicShowPage;
